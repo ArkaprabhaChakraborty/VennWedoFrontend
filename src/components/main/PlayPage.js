50ec20be1a41b5678acfd2c074ea18128cf0fd2b
ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>
2021-01-27 16:09:33 +0530

    venn wedo finalise design

--- src/components/main/PlayPage.js
+++ src/components/main/PlayPage.js
@@ -2,10 +2,44 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 import Nav from './Nav'
 import {ReactComponent as Assent} from './assent.svg'
+import ShareIcon from '@material-ui/icons/Share';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 
 import './video.css'
 const PlayPage = () => {
     let {id}  = useParams()
+    const suggestion = [
+        {
+            id:"1",
+            name:"video1",
+            cname:"channel1",
+        },
+        {
+            id:"2",
+            name:"video2",
+            cname:"channel2",
+
+        },
+        {
+            id:"6",
+            name:"video6",
+            cname:"channel6",
+        },
+        {
+            id:"3",
+            name:"video3",
+            cname:"channel3",
+        },
+        {
+            id:"4",
+            name:"video4",
+            cname:"channel4",
+        },
+        {
+            id:"5",
+            name:"video5",
+            cname:"channel5",
+        }]
     return (
         <div className="video">
             <Nav/>
@@ -15,13 +49,29 @@ const PlayPage = () => {
                     <p className="videoTitle">Video {id}</p>
                     <div className="viewcountbar">
                         <p>135002145 views</p>
-                        <Assent/>
+                        <div className="actionbar">
+                            <Assent fontSize="small"/>
+                            <ShareIcon fontSize="large"/>
+                            <FavoriteBorderIcon fontSize="large"/>
+                        </div>
                     </div>
                 </div>
                 <div className="sideband">
-                    <div className="videosuggestions">
-                        <img src="D:\\Private\Snow-Leopard-in-Himachal-feature" alt="" className="thumbnails"></img>
+                    {suggestion.map((video,i)=>(
+                    <div className="suggestionflex">
+                        <div className="videosuggestions">
+                            <img src="https://i.pinimg.com/originals/dd/a1/2c/dda12c587120e5e4378a01f31e812672.jpg" alt="" className="thumbnails"></img>
+                        </div>
+                        <div className="details">
+                            <p className="videoname">{video.name}</p>
+                            <div className="viewbar">
+                                {video.cname}
+                                <br></br>
+                                1300545 Views
+                            </div>
+                        </div>
                     </div>
+                      ))}
                 </div>
             </div>
         </div>
